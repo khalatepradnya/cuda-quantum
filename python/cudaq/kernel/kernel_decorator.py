a78b7eceed8f61e729b21996c4893eba68d8d19a
Pradnya Khalate <pkhalate@nvidia.com>
2025-02-12 12:06:40 -0800

    * Remove the Python `MidCircuitMeasurementAnalyzer` and use   `quake-add-metadata` pass instead. * Handful for state prep / init tests fail to compile due to potential   issues in capturing data from parent scope - marked them as skipped.

--- python/cudaq/kernel/kernel_decorator.py
+++ python/cudaq/kernel/kernel_decorator.py
@@ -15,7 +15,7 @@ from ..mlir.passmanager import *
 from ..mlir.dialects import quake, cc, func
 from .ast_bridge import compile_to_mlir, PyASTBridge
 from .utils import mlirTypeFromPyType, nvqppPrefix, mlirTypeToPyType, globalAstRegistry, emitFatalError, emitErrorIfInvalidPauli, globalRegisteredTypes
-from .analysis import MidCircuitMeasurementAnalyzer, HasReturnNodeVisitor
+from .analysis import HasReturnNodeVisitor
 from ..mlir._mlir_libs._quakeDialects import cudaq_runtime
 from .captured_data import CapturedDataStorage
 from ..handlers import PhotonicsHandler
@@ -159,11 +159,6 @@ class PyKernelDecorator(object):
                 'CUDA-Q kernel has return statement but no return type annotation.'
             )
 
-        # Run analyzers and attach metadata (only have 1 right now)
-        analyzer = MidCircuitMeasurementAnalyzer()
-        analyzer.visit(self.astModule)
-        self.metadata = {'conditionalOnMeasure': analyzer.hasMidCircuitMeasures}
-
         # Store the AST for this kernel, it is needed for
         # building up call graphs. We also must retain
         # the source code location for error diagnostics
@@ -209,7 +204,6 @@ class PyKernelDecorator(object):
 
         self.module, self.argTypes, extraMetadata = compile_to_mlir(
             self.astModule,
-            self.metadata,
             self.capturedDataStorage,
             verbose=self.verbose,
             returnType=self.returnType,
--- python/cudaq/runtime/sample.py
+++ python/cudaq/runtime/sample.py
@@ -7,6 +7,9 @@
 # ============================================================================ #
 from ..mlir._mlir_libs._quakeDialects import cudaq_runtime
 from .utils import __isBroadcast, __createArgumentSet
+from ..kernel.utils import nvqppPrefix
+from ..kernel.kernel_decorator import PyKernelDecorator
+from ..kernel.kernel_builder import PyKernel
 
 
 def __broadcastSample(kernel, *args, shots_count=0):
@@ -66,8 +69,15 @@ Returns:
     ctx = cudaq_runtime.ExecutionContext("sample", shots_count)
     cudaq_runtime.setExecutionContext(ctx)
 
-    if hasattr(kernel, 'metadata') and kernel.metadata.get(
-            'conditionalOnMeasure', False):
+    if isinstance(kernel, PyKernelDecorator):
+        kernel.compile()
+        for operation in kernel.module.body.operations:
+            if not hasattr(operation, 'name'):
+                continue
+            if nvqppPrefix + kernel.name == operation.name.value and 'qubitMeasurementFeedback' in operation.attributes:
+                ctx.hasConditionalsOnMeasureResults = True
+                break
+    elif isinstance(kernel, PyKernel) and kernel.conditionalOnMeasure:
         ctx.hasConditionalsOnMeasureResults = True
 
     platformSupportsConditionalFeedback = cudaq_runtime.supportsConditionalFeedback(
